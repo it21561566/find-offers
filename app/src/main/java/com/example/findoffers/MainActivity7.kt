a231c87155a403a43b4323f92411abccf7b14402
IT21301704 <99238438+IT21301704@users.noreply.github.com>
2023-05-15 11:03:29 +0530

    activity 7 button function

--- app/src/main/java/com/example/findoffers/MainActivity7.kt
+++ app/src/main/java/com/example/findoffers/MainActivity7.kt
@@ -1,11 +1,34 @@
 package com.example.findoffers
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity7 : AppCompatActivity() {
+
+    private lateinit var button:Button
+    private lateinit var button1:Button
+    private lateinit var button2:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main7)
+
+        button = findViewById(R.id.button)
+        button1 = findViewById(R.id.button5)
+        button2= findViewById(R.id.button2)
+        button.setOnClickListener{
+            val intent = Intent(this, MainActivity5::class.java)
+            startActivity(intent)
+        }
+        button1.setOnClickListener{
+            val intent = Intent(this, MainActivity5::class.java)
+            startActivity(intent)
+        }
+        button2.setOnClickListener {
+            val intent = Intent(this, MainActivity5::class.java)
+            startActivity(intent)
+        }
     }
 }
